3235c46c6e3917e1db813272b6c311b1460e9f99
joshuatucker0914 <joshuatucker0914@gmail.com>
2023-11-15 18:29:35 -0600

    complete

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,13 +1,27 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json';
 
 
-function Profile()
-{
+function Profile(){
+   const listItem = oceans.map(ocean =>
+      <div id={ocean.id} className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+          <h1>{ocean.name}</h1>
+            <img src={ocean.image} alt={ocean.name} className='oceanImg'></img>
+         <h3>Fun Facts:</h3>
+         <ul>
+            <li>{ocean.fact1}</li>
+            <li>{ocean.fact2}</li>
+            <li>{ocean.fact3}</li>
+            <Button />
+         </ul>
+      </div>
+   )
    return(
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
+         <ul>
+            {listItem}
+         </ul>
       </>  
    );
 }
